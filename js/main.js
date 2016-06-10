2f059f668ff9220edde09f0b4aca216d66d8325d
Ahmed Khaled <ahmedaka@icloud.com>
2016-06-10 03:20:05 +0200

    add nav button and menu

--- js/main.js
+++ js/main.js
@@ -5,3 +5,15 @@
 
 
 	_("banner").style.height = innerHeight + "px";
+
+
+	function navToggle(){
+
+		var nav = document.getElementById("nav");
+
+		nav.style.display = "block";
+
+		nav.onclick = function(){
+			this.style.display = "none";
+		}
+	}
