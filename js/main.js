f85a781009104194569f0b38777d8050d0624177
Ahmed Khaled <ahmedaka@icloud.com>
2016-06-12 03:10:55 +0200

    menu button

--- js/main.js
+++ js/main.js
@@ -3,17 +3,25 @@
 		return document.querySelector("#banner");
 	}
 
-
+	// makes banner's height is the window's height
 	_("banner").style.height = innerHeight + "px";
 
-
-	function navToggle(){
+	// diplay nav menu div
+	document.getElementById("menuButton").addEventListener("click", function(){
 
 		var nav = document.getElementById("nav");
 
-		nav.style.display = "block";
+		if (nav.style.display == "none") {
 
-		nav.onclick = function(){
-			this.style.display = "none";
+			this.innerHTML = "<i class='fa fa-times'></i>";
+			nav.style.display = "block";
+
+		} else {
+
+			nav.style.display = "none";
+			this.innerHTML = "<i class='fa fa-bars'></i>";
+
 		}
-	}
+
+	});
+
