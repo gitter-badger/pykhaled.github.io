b51d4effa6ba2547faec93987d22497083ec85d0
Ahmed Khaled <ahmedaka@icloud.com>
2016-07-03 01:24:33 +0200

    remove menu section and button

--- js/main.js
+++ js/main.js
@@ -3,13 +3,6 @@ $(function(){
 	// make banner image shows up slowlly
 	$(".banner__img").slideUp(100).delay(350).fadeIn(600);
 
-
-	// nav button behavior
-	$( "#menuButton" ).click(function() {
-		$("#nav").toggle(100);
-	});
-
-
 	// hide loading image after loads
 	$(window).load(function(){
 		$("#loader").hide();
